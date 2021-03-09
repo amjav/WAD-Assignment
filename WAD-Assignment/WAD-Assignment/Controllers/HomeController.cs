527de205c440c04a8568527b3fb56c1d31ca1322
Amina Javed <b9005262@my.shu.ac.uk>
2021-03-09 14:40:24 +0000

    Database Set Up
    Implemented the database. able to view all films on a webpage

--- WAD-Assignment/WAD-Assignment/Controllers/HomeController.cs
+++ WAD-Assignment/WAD-Assignment/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
 using WAD_Assignment.Models;
 
 namespace WAD_Assignment.Controllers
@@ -12,10 +15,12 @@ namespace WAD_Assignment.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -23,9 +28,15 @@ namespace WAD_Assignment.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+
+        public IActionResult AllFilms()
+
         {
-            return View();
+
+            List<Film> model = _context.Films.ToList();
+
+            return View(model);
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ WAD-Assignment/WAD-Assignment/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace WAD_Assignment.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Film> Films { get; set; }
+    }
+}
+
+
