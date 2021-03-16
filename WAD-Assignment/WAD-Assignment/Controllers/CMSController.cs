59f78e6fbd9ff33cff642fe4d8eec00bbb253a9b
Amina Javed <b9005262@my.shu.ac.uk>
2021-03-16 16:05:16 +0000

    CRUDing
    Built a content management system allows deleting, updating a film as well as adding a new film.

---
+++ WAD-Assignment/WAD-Assignment/Controllers/CMSController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WAD_Assignment.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WAD_Assignment.Controllers
+{
+    public class CMSController : Controller
+    {
+        private readonly ILogger<CMSController> _logger;
+
+        private readonly ApplicationDbContext _context;
+
+        public CMSController(ILogger<CMSController> logger, ApplicationDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Film> model = _context.Films.ToList();
+            return View(model);
+        }
+
+        // Call to a View
+        [HttpGet]
+        public IActionResult AddFilm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddFilm(FilmForm model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                Film newFilm = new Film
+                {
+                    FilmTitle = model.FilmTitle,
+                    FilmCertificate = model.FilmCertificate,
+                    FilmDescription = model.FilmDescription,
+                    FilmImage = model.FilmImage,
+                    FilmPrice = model.FilmPrice,
+                    Stars = model.Stars,
+                    ReleaseDate = DateTime.Now,
+                };
+                _context.Add(newFilm);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                //var errors = ModelState.Values.SelectMany(v => v.Errors);
+                return View();
+
+
+            }
+
+
+
+
+        }
+
+
+        //action to update the film.
+        [HttpGet]
+        public IActionResult UpdateFilm(int id)
+        {
+            Film model = _context.Films.Find(id);
+            FilmForm formModel = new FilmForm
+            {
+                FilmID = model.FilmID,
+                FilmTitle = model.FilmTitle,
+                FilmCertificate = model.FilmCertificate,
+                FilmDescription = model.FilmDescription,
+                FilmImage = model.FilmImage,
+                FilmPrice = model.FilmPrice,
+                Stars = model.Stars,
+                ReleaseDate = model.ReleaseDate,
+            };
+            ViewBag.ImageName = model.FilmImage;
+            return View(formModel);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateFilm(FilmForm model)
+        {
+            if (ModelState.IsValid)
+            {
+                Film editFilm = new Film
+                {
+                    FilmID = model.FilmID,
+                    FilmTitle = model.FilmTitle,
+                    FilmCertificate = model.FilmCertificate,
+                    FilmDescription = model.FilmDescription,
+                    FilmImage = model.FilmImage,
+                    FilmPrice = model.FilmPrice,
+                    Stars = model.Stars,
+                    ReleaseDate = model.ReleaseDate
+                };
+                _context.Films.Update(editFilm);
+
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        //Delete Film
+        [HttpGet]
+        public IActionResult DeleteFilm(int Id)
+        {
+            Film model = _context.Films.Find(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteFilm(Film model)
+        {
+            _context.Films.Remove(model);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ WAD-Assignment/WAD-Assignment/Models/FilmForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace WAD_Assignment.Models
+{
+    public class FilmForm
+
+    {
+        [Key]
+        public int FilmID { get; set; }
+
+        
+        public string FilmTitle { get; set; }
+
+        [Required]
+        public string FilmCertificate { get; set; }
+
+        public string FilmDescription { get; set; }
+
+        public string FilmImage { get; set; }
+
+        [Required]
+        public decimal FilmPrice { get; set; }
+
+        public int Stars { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+    }
+
+}
