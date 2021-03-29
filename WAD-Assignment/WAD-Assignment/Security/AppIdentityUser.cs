b03c995ffbb1313af0cd567d4a41ebe3d3fca6b0
Amina Javed <b9005262@my.shu.ac.uk>
2021-03-29 17:58:44 +0000

    Starting implementing User Types

---
+++ WAD-Assignment/WAD-Assignment/Security/AppIdentityUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WAD_Assignment.Security
+{
+    public class AppIdentityUser : IdentityUser
+    {
+    }
+}
